9b30355af1592a19be4d98c19bc153b12b0fce5d
Mathieu Mure <mathieu.mure@gmail.com>
2020-05-30 11:34:54 +0200

    config: remove github repo

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,8 +3,6 @@ module.exports = {
     description: "Je fais des trucs",
     dest: 'docs',
     themeConfig: {
-        // Assumes GitHub. Can also be a full GitLab url.
-        repo: 'mathieumure/blog',
         // if your docs are not at the root of the repo:
         docsDir: 'src',
         sidebar: 'auto',
